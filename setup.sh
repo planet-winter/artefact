6fa348203cdd03d36e3baea307bfdae511c20a5d
planet-winter <dani.winter@gmx.net>
2017-05-31 08:43:11 -0700

    signals going out

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install python-rpi.gpio
+
+sudo apt-get install python-rpi.gpio git
+
+cd /opt && git clone https://github.com/planet-winter/artefact.git artefact
+
+cd artefact
+sudo artefact.service /lib/systemd/system/artefact.service
+sudo chmod 644 /lib/systemd/system/artefact.service
+sudo systemctl daemon-reload
+sudo systemctl enable artefact.service
+sudo systemctl start artefact.service
+
+
